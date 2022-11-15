a20d9446b6fa760896f321a31d63d4390429b73b
unknown <jeantituana@hotmail.com>
2022-11-14 19:33:53 -0500

    Android Studio

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -9,6 +9,8 @@ import { Preferences } from '@capacitor/preferences';
 })
 export class PhotoService {
 
+  public photos: UserPhoto[] = [];
+
   constructor() { 
    }
 
@@ -19,6 +21,18 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+
   }
-  
+
 }
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
+}
+
